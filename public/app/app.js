79eb7303f8e18417ddff162d496ce9ccddf8c3cc
Samb101 <samuelbenais@outlook.com>
2017-12-29 19:20:24 +0100

    Remove front repo from this back repo

--- public/app/app.js
+++
@@ -1,4 +0,0 @@
-angular.module('userApp',['appRoutes','userControllers','userServices','mainController', 'authServices'])
-.config(function($httpProvider){
-  $httpProvider.interceptors.push('AuthInterceptors');
-});
